c6ac196a1f6d021060459571826fd7eb744d7e38
Compilers <compilers@compilers-vm.(none)>
2015-07-01 06:56:56 -0700

    add ex1_22

---
+++ ch1/exercises/ex1_22.cpp
@@ -0,0 +1,22 @@
+/*Date    :2015/07/01
+ *Author  :zhuyoujun
+ *Email   :zhuyoujun0513@163.com
+ *Function:intro to class: add unknown number of sales_items whose isbn is the same.
+ *Link    :C++ Primer 5th(english version)
+              @page 22,ex1_22
+ */
+
+#include<iostream>
+#include"../examples/Sales_item.h"
+
+int main()
+{
+        Sales_item item,sum;
+        
+        while(std::cin >> item)
+        {
+                sum += item;
+        }
+        std::cout << sum << std::endl; // print their sum.
+        return 0;
+}
