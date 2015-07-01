27c47eaa4ce656b5e3f5673f8c85b8dbfe5e882b
Compilers <compilers@compilers-vm.(none)>
2015-07-01 06:26:22 -0700

    add header

--- ch1/examples/eg1_1_1.cpp
+++ ch1/examples/eg1_1_1.cpp
@@ -1,3 +1,10 @@
+/*Date    :2015/06/25 
+ *Author  :zhuyoujun
+ *Email   :zhuyoujun0513@163.com
+ *Function:first c++ program.
+ *Link    :C++ Primer 5th(english version)
+              @page 3.
+ */
 int main()
 {
         return 0;
