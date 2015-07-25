510b6e0de988087b25e7a8987e49fd4a4d9e1de7
zhuyoujun <zhuyoujun0513@163.com>
2015-07-24 19:39:51 -0700

    update

--- ch4/examples/eg4_4_1.cpp
+++ ch4/examples/eg4_4_1.cpp
@@ -1,9 +1,9 @@
 /*Date    :2015/07/23
  *Author  :zhuyoujun
  *Email   :zhuyoujun0513@163.com
- *Function:logic and relational operators.
+ *Function:assignment operators.
  *Link    :C++ Primer 5th(english version)
-              @page 141, $4.3 section
+              @page 143, $4.4 section
  */
 
 #include<iostream>
@@ -15,33 +15,53 @@ using std::cout;
 using std::endl;
 int main()
 {
-        // or "||" operator.
-        vector<string> text;
-        for (const auto &s : text)
+        int i = 0, j = 0, k = 0;
+        const int ci = i;
+        
+        //error
+        1024 = k;
+        i + j = k;
+        ci = k;
+
+        //
+        k = 0;
+        k = 3.1415926; //warning
+
+        k = {3.14}; //error
+        vector<int> vi;
+        vi = {0,1,2,3,4,5,6,7,8,9};
+
+        int ival,jval;
+        ival = jval = 0;
+        int ival, *pval;
+        ival = pval = 0; //error
+
+        string s1, s2;
+        s1 = s2 = "OK";
+
+        int i = get_value();
+        while (i != 42)
         {
-                if (s.empty() || s[s.size()-1] == '.')
-                        cout << endl;
-                else
-                {
-                        cout << s;
-                }
+                i = get_value();
         }
 
-        // not operator
-        vector<int> vec = {0,1,2};
-        if (!vec.empty())
+        int j;
+        while ((j = get_value()) != 42)
         {
-                cout << vec[0];
+                //do sth
         }
         
-        //relation operator.
-        int i , j ,k ;
-        if (i < j < k)   // i < j => true => 1<k;
-        if (i < j && j < k)
-        int val;
-        if (val)
-        if (!val)
+        if (i = j){
+        }
 
+        if (i == j){
+        }
+
+        int sum = 0;
+        for (int val = 1; val != 10; val++)
+        {
+                sum += val;
+        }
 
         return 0;
 }
